bb842b85bbcb8490abe21fa43e392af0dba18be5
ANDRES SAENZ <SAENZA76@YAHOO.COM>
2016-11-10 14:28:39 -0600

    getting server.js and routing initialized

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+app.use(express.static('app'));
+require('./public/routing/html-routes.js')(app);
+
+app.listen(process.env.PORT, function(){
+	console.log('Listening on PORT ' + PORT);
+});
